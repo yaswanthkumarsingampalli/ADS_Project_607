4f392eac318d40130b3a7ffb1d1ac6021f72c1f3
yaswanthkumarsingampalli <47891789+yaswanthkumarsingampalli@users.noreply.github.com>
2023-12-08 15:21:36 -0600

    Update Data_cleaning_file.py

--- pythonfiles/Data_cleaning_file.py
+++ pythonfiles/Data_cleaning_file.py
@@ -20,7 +20,6 @@ def save_cleaned_dataset(cleaned_data, local_output_path):
     cleaned_data.to_csv(local_output_path, index=False)
 
 def push_to_github(local_repo_path, local_output_path, commit_message):
-    print(local_repo_path)
     # Change to the directory of the local GitHub repository
     os.chdir(local_repo_path)
 
@@ -32,14 +31,12 @@ def push_to_github(local_repo_path, local_output_path, commit_message):
 
     # Push the changes to the GitHub repository
     os.system('git push')
-    print('git push')
-
 
 if __name__ == "__main__":
     # GitHub URLs for input and local output paths
     input_url = "https://github.com/yaswanthkumarsingampalli/ADS_Project_607/raw/main/Data_sets/googleplaystore.csv"
     local_output_path = "cleaned_dataset.csv"
-    local_repo_path = "./"  # Change this to your local GitHub repository path
+    local_repo_path = "https://github.com/yaswanthkumarsingampalli/ADS_Project_607/raw/main/Data_sets"  # Change this to your local GitHub repository path
 
     # Download the dataset from the input repository
     raw_data = download_dataset(input_url)
